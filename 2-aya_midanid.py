66e7a676b77b61537cff1a4a43dc5bd4c6327f78
MMD <mmd.gh313@gmail.com>
2025-10-03 18:58:44 +0330

    Add the first ones

---
+++ 2-aya_midanid.py
@@ -0,0 +1,5 @@
+print('''programming statistics:
+world population is 8.17 billion.
+programming percentage is 3.87 percent.
+average of program learner per year is 22.69 million.
+average age of programming learners is 23.73 year.''')
